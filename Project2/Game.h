6cfc5bbbe8fa965adbc3d8bca68cc1ae05901d8a
mahm99 <59301064+mahm99@users.noreply.github.com>
2020-02-12 17:57:35 -0500

    Update Game.h

--- Project2/Game.h
+++ Project2/Game.h
@@ -1,10 +1,7 @@
 /**********************************************************
  * File:        Game.h
- * Project:     CMSC 202, Project 02, Fall 2019
  * Author:      Mahmood Ul Mohasin Aziz
- * Date:        10/10/2019
- * Section:     13
- * Email:       rg66173@umbc.edu
+ * Description: Header file for the Game class.
  ********************************************************/
 
 #include <iostream>
