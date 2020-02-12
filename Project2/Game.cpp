8b57921d495571513954529ac5ded7de4ed7eaa4
mahm99 <59301064+mahm99@users.noreply.github.com>
2020-02-12 17:55:54 -0500

    Update Game.cpp

--- Project2/Game.cpp
+++ Project2/Game.cpp
@@ -1,11 +1,8 @@
 /**********************************************************
 * File:        Game.cpp
-* Project:     CMSC 202, Project 02, Fall 2019
 * Author:      Mahmood Ul Mohasin Aziz
-* Date:        10/10/2019
-* Section:     13
-* Email:       rg66173@umbc.edu
 * 
+* Description: 
 * This file contains c++ program code for the class Game
 * that determines how the game is being played, provides 
 * user with menu options and asks them choose what they 
