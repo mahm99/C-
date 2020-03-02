4c95dd77aa824cd7bd2456174512175cf53d98bf
mahm99 <59301064+mahm99@users.noreply.github.com>
2020-03-02 10:53:28 -0500

    Create Paladin.h

---
+++ Project4/Paladin.h
@@ -0,0 +1,36 @@
+/******************************************************
+* File: Paladin.h                                                                              
+* Author: Mahmood Ul Mohasin Aziz                                                                                                                                            
+* Description:                                                                                     
+* This is the header file for the Paladin class. 
+*****************************************************/
+
+#ifndef _PALADIN_H_
+#define _PALADIN_H_
+
+#include "Character.h"
+
+class Paladin: public Character {
+ public:
+  // Name: Paladin() - Default Constructor
+  // Description: Creates a new paladin
+  // Preconditions: None
+  // Postconditions: Can create a paladin
+  Paladin();
+  // Name: Paladin(string name, int hp)
+  // Description: Creates a new paladin
+  // Preconditions: None
+  // Postconditions: Can create a paladin
+  Paladin(string, int);
+  // Name: ~Paladin - Virtual Destructor
+  // Description: Makes sure everything in child class is deallocated
+  // Preconditions: None
+  // Postconditions: Everything dynamically allocated is deallocated
+  ~Paladin();
+  // Name: SpecialAttack
+  // Description: Defines the Paladin's special attack
+  // Preconditions: None
+  // Postconditions: Returns damage from special attack
+  int SpecialAttack();
+};
+#endif
